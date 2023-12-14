6cf9b86f96180cb10fe3631d3fc81e52e28b92cb
Tarik364 <57274119+Tarik364@users.noreply.github.com>
2023-12-14 23:30:54 +0300

    Add files via upload

---
+++ Chat_App/Chat_App/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace Chat_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.White;
+            this.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.Appearance.Options.UseBackColor = true;
+            this.Appearance.Options.UseForeColor = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 469);
+            this.IconOptions.ColorizeInactiveIcon = DevExpress.Utils.DefaultBoolean.False;
+            this.IconOptions.ShowIcon = false;
+            this.IsMdiContainer = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "x\"";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+    }
+}
+
